d2ca0591e5f38ebeb56a631ef62ab6fde853e3bd
Nikos Gianniotis <ngiann@gmail.com>
2024-04-09 10:48:12 +0200

    wrote tests

--- src/ExponentialExpectations.jl
+++ src/ExponentialExpectations.jl
@@ -1,7 +1,13 @@
 module ExponentialExpectations
 
+    using Random, QuadGK, Distributions, Printf, HCubature
+
     include("expectations_exponential.jl")
 
     export E, B, V, Elognormal
+
+    include("long_tests.jl")
+
+    export long_tests
 
 end
